4e1225fcc918b320077bf82417c8bb194816911f
Jake Garcia <bballwiz4@gmail.com>
2019-09-23 15:50:41 -0700

    App is able to identify dogs, cats, rabbits, and humans

--- Recognize/ViewController.swift
+++ Recognize/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController:UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
@@ -27,8 +29,61 @@ class ViewController:UIViewController, UINavigationControllerDelegate, UIImagePi
         present(imagePicker, animated: true)
         
     }
+    // MARK: - TODO
     
+    // didFinishPickingMediaWithOptions
+
+    // create func to detect what the chosen image is.
     
+    // MARK: - Did Finish Pickinf Media
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        guard let userEditedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage else {
+            fatalError("Unable to get the edited image")
+        }
+        
+        if let convertedImage = CIImage(image: userEditedImage) {
+            detect(userImage: convertedImage)
+        }
+        
+        imageTakenView.image = userEditedImage
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+        
+    }
+    
+    func detect(userImage: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: first_Image_Classifier().model) else {
+            fatalError("Could not initialize vision model with given model.")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Unable to retrieve classification observations")
+            }
+            
+            if let firstResult = results.first {
+                print(firstResult.identifier)
+                self.title = "It's a \(firstResult.identifier)"
+            }
+            
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: userImage)
+        
+        do {
+            try! handler.perform([request])
+        } catch {
+            print("Error: \(error)")
+        }
+        
+        
+        
+        
+    }
     
 }
 
